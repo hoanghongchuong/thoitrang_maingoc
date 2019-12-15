45248401aaa4fcc55db8ad4c76e00fbc6230db4c
hoanghongchuong <chuonghoanghong@gmail.com>
2019-12-15 23:32:45 +0700

    a

--- js/script.js
+++ js/script.js
@@ -1,45 +1,27 @@
 $(document).ready(function($){
-	var bannerFullHeight = function(){
-		var sidebar_child = $('.sidebar-child').height();
-        var sidebar = $('.menu-sidebar').height();
-        // alert(sidebar);
-        sidebar = sidebar_child;
-    }();
+	var owl = $(".owl-carousel");
+        owl.owlCarousel({
+            margin:0,                           
+            loop:true,
+            dots:true,
+            autoplay:false,                         
+            autoplayTimeout:1500,
+            dotsEach: true,
+            dotsData: true,
+            autoplayHoverPause:true,
+            autoplaySpeed: 2000,
+            responsiveClass:true,               
+            responsive:{
+                0:{
+                    items:2,                                                                                                
+                },
+                600:{
+                    items:4,          
+                },
+                1000:{
+                    items:4,  
+                }
+            }
+        });
 
-	var owl = $(".owl-carousel-product");
-	  	owl.owlCarousel({
-	  		margin:0, 							
-	  		loop:true, 							
-	  		nav:true, 							
-	  		navText:['<i class="fa fa-angle-left"></i>','<i class="fa fa-angle-right"></i>'], 
-	  		autoplay:false, 						
-	  		autoplayTimeout:1500,
-			autoplayHoverPause:true,
-			autoplaySpeed: 2000,
-			responsiveClass:true, 				
-		    responsive:{
-		        0:{
-		            items:2,									            									            
-		        },
-		        600:{
-		            items:4,          
-		        },
-		        1000:{
-		            items:4,  
-		        }
-		    }
-		});
-
-	$('.show-box-search').on('click', function(){
-		$('.box-search').toggle();
-	});
-	// menu scroll
-	$(window).scroll(function(){
-	    if ($(window).scrollTop() >= 300) {
-	        $('.menu').addClass('fixed-menu');        
-	    }
-	    else {
-	        $('.menu').removeClass('fixed-menu');        
-	    }
-	});
 });
